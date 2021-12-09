aa2acf38640d6660f48cb6e3e5703969f494c834
Gilmar <gilmar.fer10@gmail.com>
2021-12-09 14:44:34 -0300

    go

--- dist/db/mongo/mongoseConnect.js
+++ dist/db/mongo/mongoseConnect.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable @typescript-eslint/no-explicit-any */
 var mongoose_1 = __importDefault(require("mongoose"));
-var uri = 'mongodb://127.0.0.1:27017/local';
-// const pass = process.env.MONG_PASS;
-// const dbName = process.env.DB_NAME;
-// const uri = `mongodb+srv://mo:${pass}@mo.73ws7.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+// const uri = 'mongodb://127.0.0.1:27017/local';
+var pass = process.env.MONG_PASS;
+var dbName = process.env.DB_NAME;
+var uri = "mongodb+srv://mo:" + pass + "@mo.73ws7.mongodb.net/" + dbName + "?retryWrites=true&w=majority";
 var conn = mongoose_1.default.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
     if (err) {
         console.log(err.message);
--- src/db/mongo/mongoseConnect.ts
+++ src/db/mongo/mongoseConnect.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from 'mongoose';
 
-const uri = 'mongodb://127.0.0.1:27017/local';
-// const pass = process.env.MONG_PASS;
-// const dbName = process.env.DB_NAME;
-// const uri = `mongodb+srv://mo:${pass}@mo.73ws7.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+// const uri = 'mongodb://127.0.0.1:27017/local';
+const pass = process.env.MONG_PASS;
+const dbName = process.env.DB_NAME;
+const uri = `mongodb+srv://mo:${pass}@mo.73ws7.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 const conn = mongoose.connect(
   uri,
